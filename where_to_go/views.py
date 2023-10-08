2b40b94db69f096f8e2c04e0dc8fa45a40e9752d
romanulanov <turnsourcream@gmail.com>
2023-10-08 16:16:54 +0400

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
-from places.models import Place
+from places.models import Place, Image
+import json
 
 
-dict = {
+
+places = Place.objects.all()
+
+for id, place in enumerate(places):
+    place_details = {}
+    place_details["title"] = place.title
+    place_details["description_short"] = place.description_short
+    place_details["description_long"] = place.description_long
+    place_details["coordinates"] = {}
+    place_details["coordinates"]["lng"] = place.lon
+    place_details["coordinates"]["lat"] = place.lat
+    place_details["imgs"] = []
+    images = Image.objects.filter(title__title__contains =place.title)
+    for image in images:
+      place_details["imgs"].append(f"{image.img.url}") 
+    place_details_json = json.dumps(place_details)
+
+    with open(f"static/places/{id}.json", "w") as file:
+        file.write(place_details_json)
+    
+    
+place_details = {
       "type": "FeatureCollection",
       "features": [
         {
@@ -15,7 +37,7 @@ dict = {
           "properties": {
             "title": "Легенды Москвы",
             "placeId": "moscow_legends",
-            "detailsUrl": "{% static '/places/moscow_legends.json' %} "
+            "detailsUrl": "static/places/0.json"
           }
         },
         {
@@ -27,11 +49,11 @@ dict = {
           "properties": {
             "title": "Крыши24.рф",
             "placeId": "roofs24",
-            "detailsUrl": "{% static '/places/roofs24.json' %} "
+            "detailsUrl": "static/places/1.json"
           }
         }
       ]
     }
-data = {"dict": dict}
+data = {"dict": place_details}
 def index(request):
     return TemplateResponse(request,  "index.html", context=data)
