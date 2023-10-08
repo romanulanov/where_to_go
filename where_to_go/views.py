c649fbc86272cca72e223bd4440c7bc318ce113e
Уланов Роман <gogon1995@mail.ru>
2023-10-08 23:58:25 +0400

    did easier adding photos; returned json in placeview

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -42,6 +42,22 @@ def index(request):
 
 
 def places(request, place_id):
+
+
+    
     place = get_object_or_404(Place.objects.all(), pk=place_id)
+    place_details = dict()
+    title_short = place.title[place.title.find("«") + 1: place.title.find("»")]
+    place_details["title"] = title_short
+    place_details["description_short"] = place.description_short
+    place_details["description_long"] = place.description_long
+    place_details["coordinates"] = {}
+    place_details["coordinates"]["lng"] = place.lon
+    place_details["coordinates"]["lat"] = place.lat
+    place_details["imgs"] = []
+    images = Image.objects.filter(title__title__contains =place.title)
+    for image in images:
+      place_details["imgs"].append(f"{image.img.url}") 
+    #place_details_json = json.dumps(place_details)
     context = {"place":place}
-    return render(request, 'place-details.html', context)
+    return JsonResponse(place_details)
