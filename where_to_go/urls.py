c7feae81959f2c5eeb49cd98b9a8b523ae1720af
romanulanov <turnsourcream@gmail.com>
2023-11-28 13:31:10 +0400

    added reverse

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -19,12 +19,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from .views import index, places
+from . import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('places/<int:place_id>/', places, name = 'place-archive')
+    path('', views.index),
+    path('places/<int:place_id>/', views.places, name = 'place-archive')
     
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -24,45 +24,42 @@ for id, place in enumerate(places):
     for image in images:
         place_details["imgs"].append(f"{image.img.url}") 
     place_details_json = json.dumps(places_details)
-print(place_details)    
+   
 
 
 def places(request, place_id):
     place = get_object_or_404(Place.objects.all(), pk=place_id)
     place_details = dict()
     title_short = place.title[place.title.find("«") + 1: place.title.find("»")]
-    place_details["title"] = title_short
-    place_details["description_short"] = place.description_short
-    place_details["description_long"] = place.description_long
-    place_details["coordinates"] = {}
-    place_details["coordinates"]["lng"] = place.lon
-    place_details["coordinates"]["lat"] = place.lat
-    place_details["imgs"] = []
-    
+    place_details = {
+                    "title" : title_short,
+                    "description_short" : place.description_short,
+                    "description_long" : place.description_long,
+                    "coordinates": {"lng" : place.lon,
+                                    "lat" : place.lat},
+                    "imgs" : []
+                    }
     images = Image.objects.filter(title__title__contains =place.title)
     for image in images:
       place_details["imgs"].append(f"{image.img.url}") 
     return JsonResponse(place_details, safe=False, json_dumps_params={'indent': 2, 'ensure_ascii': False})
 
 
-
-
-
-'''
-places_details = []
-for place in range(2):
-    places_details.append()
-
-'''
-
-place_details = []
-for id, place in places_details:
-    #title_short = place.title[place.title.find("«") + 1: place.title.find("»")]
-    place_details.append(Feature(geometry=Point((place.lat, place.lon)), properties = {"title":title_short, "placeId":id, "detailsUrl": places(id)}))
-place_details = FeatureCollection(place_details)  
-
-data = {"dict": place_details}
+def get_GeoJSON(request):
+    places = Place.objects.all()
+    place_details = []
+    for id, place in enumerate(places):
+        title_short = place.title[place.title.find("«") + 1: place.title.find("»")]
+        place_details.append(Feature(geometry=Point((place.lat, place.lon)), 
+                                    properties = {"title":title_short,
+                                                "placeId":id,
+                                                "detailsUrl": reverse('place-archive', kwargs={'place_id':id})
+                                                }
+                                                ))                                       
+    place_details = FeatureCollection(place_details)
+    return place_details
 
 
 def index(request):
+    data = {"dict": get_GeoJSON}
     return TemplateResponse(request,  "index.html", context=data)
