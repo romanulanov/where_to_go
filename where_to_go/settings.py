e22a1245f20d6bce031b215f25985203db2f72f5
romanulanov <turnsourcream@gmail.com>
2024-02-07 19:11:56 +0400

    меньше настроек CLI

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
