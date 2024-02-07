b231114a2c105bc5fce4a6fecd2db337659bd1ce
romanulanov <turnsourcream@gmail.com>
2024-02-07 19:14:13 +0400

    Переместил views в places

--- places/views.py
+++ places/views.py
@@ -1,1 +1,57 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from geojson import Feature, Point, FeatureCollection
+from django.template.response import TemplateResponse
+from places.models import Place, Image
+
+
+def places(request, place_id):
+    place = get_object_or_404(Place.objects.all(), pk=place_id)
+    place_details = dict()
+    title_short = place.title
+    place_details = {
+                    "title": title_short,
+                    "description_short": place.description_short,
+                    "description_long": place.description_long,
+                    "coordinates": {"lng": place.lon,
+                                    "lat": place.lat},
+                    "imgs": []
+                    }
+    images = Image.objects.filter(title__title__contains=place.title)
+    for image in images:
+        place_details["imgs"].append(f"{image.img.url}")
+    return JsonResponse(
+        place_details,
+        safe=False,
+        json_dumps_params={
+                           'indent': 2,
+                           'ensure_ascii': False
+                           }
+                        )
+
+
+def index(request):
+    places = Place.objects.all()
+    place_details = []
+    for id, place in enumerate(places, start=1):
+        title_short = place.title
+        place_details.append(Feature
+                             (geometry=Point((place.lat, place.lon)),
+                                 properties={
+                                    "title": title_short,
+                                    "placeId": id,
+                                    "detailsUrl": reverse(
+                                        'place-archive',
+                                        kwargs={'place_id': id}
+                                                )
+                                    }
+                              )
+                             )
+
+    place_details = FeatureCollection(place_details)
+    return TemplateResponse(request,
+                            "index.html",
+                            context={"dict": place_details}
+                            )
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
+from places import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
